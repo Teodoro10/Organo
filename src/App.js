dff0aa40b44c8ac2658343244a0c25d4d358e087
Teodoro10 <feliperibe10@outlook.com>
2023-03-13 13:13:31 -0300

    first step

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Banner from "./componete/Banner/Banner.js";
+
+function App() {
+  return (
+    <div className="App">
+      <Banner/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componete/Banner/Banner.js
@@ -0,0 +1,11 @@
+import "./Banner.css";
+
+function banner(){
+    return(
+        <header className="Banner">
+            <img src="./imagens/banner.png" alt="Banner da marca"/>
+        </header>
+    )
+}
+
+export default banner;
